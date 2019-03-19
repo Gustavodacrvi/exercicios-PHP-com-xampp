fb5d1a753f336c548e22eaee9aaab63aecaeb4f8
gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-19 09:29:18 -0300

    codigo

--- exercicio4/php.php
+++ exercicio4/php.php
@@ -1,1 +1,19 @@
-f
+<?php
+
+$limite = $_POST['numero'];
+
+$i = 1;
+$response = '';
+while ($i < $limite) {
+	if ($i % 2 == 0) {
+		$response .= $i;
+	} else {
+		$response .= '1';
+	}
+	$response .= ' ';
+	$i += 1;
+}
+
+echo $response;
+
+?>
