7df1b48f98074e44c11afb6523e6649581fba4e3
gustavodacrvi <gustavodacrvi@gmail.com>
2019-03-19 09:07:28 -0300

    codigo fibonacci

--- exercicio2/php.php
+++ exercicio2/php.php
@@ -1,1 +1,19 @@
-dfs
+<?php
+
+$limite = $_POST['numero'];
+
+$fib = '1 1';
+$ant = 1;
+$pos = 1;
+while (true) {
+	$temp = $pos;
+	$pos = $ant + $pos;
+	$ant = $temp;
+	if ($pos < $limite)
+		$fib .= ' '.$pos;
+	else break;
+}
+
+echo $fib;
+
+?>
