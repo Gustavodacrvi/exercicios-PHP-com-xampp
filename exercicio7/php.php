c7aeaab9c515f96b0b0b34a0de62e4a31aee57c3
Gustavodacrvi <gustavodacrvi@gmail.com>
2019-04-26 17:17:35 -0300

    code logic

--- exercicio7/php.php
+++ exercicio7/php.php
@@ -2,6 +2,24 @@
 
 $numero = $_POST['entrada'];
 
-echo $numero;
+$i = 2;
+$primo = true;
+while (true) {
+  if ($numero == $i) {
+    break;
+  }
+  if ($numero % $i == 0) {
+    $primo = false;
+    break;
+  }
+  $i++;
+}
+
+
+if ($primo) {
+  echo 'O número dado é primo';
+} else {
+  echo 'O número dado não é primo';
+}
 
 ?>
